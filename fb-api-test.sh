a061fc7f694de5314b29bf52d49cc9cd4873ce81
liao <liaomovie2@gmail.com>
2018-11-22 16:14:34 +0800

    test new line and tag

--- fb-api-test.sh
+++ fb-api-test.sh
@@ -12,13 +12,14 @@ function urlencode() {
 
 
 ## Variables
-message='部落客%0A #R #R部落'
+message='部落客'
+tag='#R #R部落'
 link='https://liao961120.github.io/2018/09/09/linguistics-down.html'
 
 
 ## Post on Facebook
 curl -i -X POST \
- "https://graph.facebook.com/v3.2/twRblogger/feed?message=$( urlencode "$message" )&link=$( urlencode "$link" )&access_token=$( urlencode "$fbtoken" )"
+ "https://graph.facebook.com/v3.2/twRblogger/feed?message=$( urlencode "${message}" )%0A"${tag}"&link=$( urlencode "$link" )&access_token=$( urlencode "$fbtoken" )"
  
  
 # ?published=false&message=$( rawurlencode "$message" )&scheduled_publish_time=+"${min}" minutes
