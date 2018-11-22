b74cccf7a45ccab3e69afa55ebb56554616a0d45
liao <liaomovie2@gmail.com>
2018-11-22 19:01:21 +0800

    clean up repo [skip ci]

--- get_info.sh
+++ get_info.sh
@@ -5,9 +5,9 @@ curl https://raw.githubusercontent.com/Rbloggers/web/posts/FB_link.txt > FB_link
 curl https://raw.githubusercontent.com/Rbloggers/web/posts/FB_tags.txt > FB_tags.txt
 
 
-sed "s/\(,\)$//" FB_tags.txt > temp  # remove trailing comma
+sed "s/\(,\)$//" FB_tags.txt > temp   # remove trailing comma
 sed "s/ /_/g" temp > temp2            # sub space(in tag) with _
-sed "s/,/ #/g" temp2 > temp3         # sub comma with space+hash
-sed "s/^/#/" temp3 > FB_tags.txt     # Add leading hash
+sed "s/,/ #/g" temp2 > temp3          # sub comma with space+hash
+sed "s/^/#/" temp3 > FB_tags.txt      # Add leading hash
 
 rm -r temp*
