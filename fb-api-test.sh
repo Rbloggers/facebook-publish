5fe7f2a5df01fb63e4eca4631294c29c5bee6cd1
liao <liaomovie2@gmail.com>
2018-11-22 17:56:46 +0800

    test scheduled post

--- fb-api-test.sh
+++ fb-api-test.sh
@@ -12,17 +12,17 @@ function urlencode() {
 
 
 ## Variables
-message='部落客 English'
-tag='#R #R 部落'
+title='部落客 English'
+tags='#R #R部落'
 link='https://liao961120.github.io/2018/09/09/linguistics-down.html'
-
+min=0
 
 ## Post on Facebook
 curl -i -X POST \
- "https://graph.facebook.com/v3.2/twRblogger/feed?message=$( urlencode "${message}" )%0A$( urlencode "${tag}" )&link=$( urlencode "${link}" )&access_token=$( urlencode "${fbtoken}" )"
+ "https://graph.facebook.com/v3.2/twRblogger/feed?published=false&message=$( urlencode "${title}" )%0A$( urlencode "${tags}" )&link=$( urlencode "${link}" )&access_token=$( urlencode "${fbtoken}" )&scheduled_publish_time=+"${min}" minutes"
  
  
-# ?published=false&message=$( rawurlencode "$message" )&scheduled_publish_time=+"${min}" minutes
+# ?published=false&message=$( rawurlencode "$title" )&scheduled_publish_time=+"${min}" minutes
  
 #https://developers.facebook.com/docs/pages/publishing?locale=zh_TW
  
--- readfile.sh
+++ readfile.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
+
+function urlencode() {
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf "$c" | xxd -p -c1 | while read x;do printf "%%%s" "$x";done
+        esac
+    done
+}
+
+fbtoken="fbtoken"
+min=0
 while IFS=$'\t' read -r title tags link
 do
-  tags='#'"${tags}"
-  printf "$title\n"
-  printf "$tags\n"
-  printf "$link\n\n"
+    printf "$min\n"
+    printf "$title\n"
+    printf "$tags\n"
+    printf "$link\n\n"
+
+    ## Post on Facebook
+    curl -i -X POST \
+     "https://graph.facebook.com/v3.2/twRblogger/feed?message=$( urlencode "${title}" )%0A$( urlencode "${tags}" )&link=$( urlencode "${link}" )&access_token=$( urlencode "${fbtoken}" )"
+  
+    min=$((${min} + 1))
+    sleep 1
 done < <(paste FB_title.txt FB_tags.txt FB_link.txt)
 
