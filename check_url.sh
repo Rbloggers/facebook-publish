3b64e26e1ff9456f6a2f6abf37c0e3bdfed15aee
liao <liaomovie2@gmail.com>
2018-11-23 21:45:20 +0800

    test cross-24hr delay publish when too many new posts

--- check_url.sh
+++ check_url.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sleep 0.5
+echo "START CHECKING"
 
 RED='\033[0;31m'
 NC='\033[0m'
@@ -7,7 +9,6 @@ error=0
 ## Check existance of new post
 [ -s FB_title.txt ] || printf "No new post.\nNo curl request sent.\n" && exit 0
 
-echo "START CHECKING"
 while read url; do
     status=$(curl -Is "${url}" | head -1 | grep -o "200")
     if [[ ${status} != "200" ]]; then
