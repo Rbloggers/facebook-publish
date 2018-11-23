1564b424722efd42a2fef3595d7b45d3c627e5e4
liao <liaomovie2@gmail.com>
2018-11-23 21:27:57 +0800

    test cross-24hr delay publish when too many new posts

--- publish.sh
+++ publish.sh
@@ -15,19 +15,28 @@ function urlencode() {
 
 ## Post on Facebook
 min=30  # First post scheduled in $min minutes
-
+postperday=5
+num=0
+day=0
 while IFS=$'\t' read -r title tags link
 do
+    num=$((${num} + 1))
+    
     # Deal with empty tags
     if [[ $tags == "#" ]]; then tags=""; fi  
     
     date=$(echo $(date --date="+${min} minutes" +%s))
-    printf "$date\n"
     
     curl -i -X POST \
      "https://graph.facebook.com/v3.2/twRblogger/feed?published=false&message=$( urlencode "${title}" )%0A$( urlencode "${tags}" )&link=$( urlencode "${link}" )&access_token=$( urlencode "${fbtoken}" )&scheduled_publish_time=${date}"
 
-    min=$((${min} + 8))
+    printf "\nPost: ${num}\n"
+    # Push publish date furthur if too many posts
+    if [[ ${num} -gt ${postperday} ]]; then
+        day=$((${num} - ${postperday}))
+    fi
+    
+    min=$((${min} + 8 + 1440 * ${day}))
     sleep 0.3
 done < <(paste FB_title.txt FB_tags.txt FB_link.txt)
 
